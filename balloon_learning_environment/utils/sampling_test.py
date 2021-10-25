ea3e4dea51d4daa7381a19b65553f2526d0b4760
Joshua Greaves <joshgreaves@google.com>
2021-10-25 21:26:32 +0000

    correct docstring reference.
    PiperOrigin-RevId: 405490965

--- balloon_learning_environment/utils/sampling_test.py
+++ balloon_learning_environment/utils/sampling_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for balloon_learning_environment.google.sampling."""
+"""Tests for balloon_learning_environment.utils.sampling."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
